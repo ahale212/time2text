aa9674b012135ecfcbe2a91b62e3861683df7dbe
Keith Magill <kmagi@allstate.com>
2016-10-31 16:25:00 +0000

    my own work on time2text

--- 2016-10-25-time2text/test/time2text_test.js
+++ 2016-10-25-time2text/test/time2text_test.js
@@ -4,5 +4,43 @@ const expect = require('chai').expect
 const time2text = require('../time2text')
 
 describe('Time to Text converter', () => {
-
+    it('Time is 00:00 expects midnight', () => {
+        expect(time2text('00:00')).to.equal('midnight')
+    })
+    it('Time is 12:00 expects midday', () => {
+        expect(time2text('12:00')).to.equal('noon')
+    })
+    it("Time is 01:00 expects one o'clock in the morning", () => {
+        expect(time2text('01:00')).to.equal("one o'clock in the morning")
+    })
+    it("Time is 02:00 expects two o'clock in the morning", () => {
+        expect(time2text('02:00')).to.equal("two o'clock in the morning")
+    })
+    it("Time is 03:00 expects three o'clock in the morning", () => {
+        expect(time2text('03:00')).to.equal("three o'clock in the morning")
+    })
+    it("Time is 04:00 expects four o'clock in the morning", () => {
+        expect(time2text('04:00')).to.equal("four o'clock in the morning")
+    })
+    it("Time is 05:00 expects five o'clock in the morning", () => {
+        expect(time2text('05:00')).to.equal("five o'clock in the morning")
+    })
+    it("Time is 06:00 expects six o'clock in the morning", () => {
+        expect(time2text('06:00')).to.equal("six o'clock in the morning")
+    })
+    it("Time is 07:00 expects seven o'clock in the morning", () => {
+        expect(time2text('07:00')).to.equal("seven o'clock in the morning")
+    })
+    it("Time is 08:00 expects eight o'clock in the morning", () => {
+        expect(time2text('08:00')).to.equal("eight o'clock in the morning")
+    })
+    it("Time is 09:00 expects nine o'clock in the mornin", () => {
+        expect(time2text('09:00')).to.equal("nine o'clock in the morning")
+    })
+    it("Time is 10:00 expects ten o'clock in the morning", () => {
+        expect(time2text('10:00')).to.equal("ten o'clock in the morning")
+    })
+    it("Time is 11:00 expects eleven o'clock in the morning", () => {
+        expect(time2text('11:00')).to.equal("eleven o'clock in the morning")
+    })
 })
--- 2016-10-25-time2text/time2text.js
+++ 2016-10-25-time2text/time2text.js
@@ -1,8 +1,80 @@
 'use strict'
 
-function time2text (time) {
+function time2text(time) {
+    var hour = time.substring(0, 2)
+    var minute = time.substring(3, 5)
+    var am = morn_or_night(hour)
+    var oClock = is_it_oclock(minute)
 
+    console.log('hour', hour, 'minute', minute)
+
+    if (time === '01:00') {
+        return "one " + oClock + am
+
+    }
+    if (time === '02:00') {
+        return "two " + oClock + am
+
+    }
+    if (time === '03:00') {
+        return "three " + oClock + am
+
+    }
+    if (time === '04:00') {
+        return "four " + oClock + am
+
+    }
+    if (time === '05:00') {
+        return "five " + oClock + am
+
+    }
+    if (time === '06:00') {
+        return "six " + oClock + am
+
+    }
+    if (time === '07:00') {
+        return "seven " + oClock + am
+
+    }
+    if (time === '08:00') {
+        return "eight " + oClock + am
+
+    }
+    if (time === '09:00') {
+        return "nine " + oClock + am
+
+    }
+    if (time === '10:00') {
+        return "ten " + oClock + am
+
+    }
+    if (time === '11:00') {
+        return "eleven " + oClock + am
+
+    }
+    if (time === '12:00') {
+        return 'noon'
+    } else {
+        return ('midnight')
+    }
 
 }
+
+function morn_or_night(hour) {
+    if (hour[0] >= 0 && hour[0] < 12) {
+        return "in the morning"
+    } else if (hour[0] > 12 && hour[0] < 18) {
+        return "in the afternoon"
+    } else
+        return "in the evening"
+}
+
+function is_it_oclock(minute) {
+    if (minute[0] === '0' && minute[1] === '0') {
+        return "o'clock "
+    }
+}
+
+
 
 module.exports = time2text
